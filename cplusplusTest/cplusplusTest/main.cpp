7fcfe8bf94c752a4c6d0465e45f6b666fb3ed52d
SangDesu <done.sang@gmail.com>
2017-12-13 17:23:22 +0800

    c++ 11 learning

--- cplusplusTest/cplusplusTest/main.cpp
+++ cplusplusTest/cplusplusTest/main.cpp
@@ -8,7 +8,9 @@
 
 #include <string>
 #include <iostream>
+#include <vector>
 #include <type_traits>
+#include <stdexcept>
 
 template<typename _T>
 struct has_func {
@@ -70,6 +72,94 @@ void ooxx(void) {
     
 }
 
+int test(int v) {
+    return ++v;
+}
+
+template<int value>
+int test1(void) {
+    return value + 1;
+}
+
+int fact(int val) {
+    return val > 1 ? fact(val - 1) * val : 1;
+}
+
+namespace ns1 {
+    void nsprint(int val) {
+        std::cout << "print1 = " << val << std::endl;
+    }
+}
+
+namespace ns2 {
+    void nsprint(char c) {
+        std::cout << "print2 = " << c << std::endl;
+    }
+}
+
+using namespace ns1;
+using namespace ns2;
+
+constexpr int new_sz(void) { return 45; }
+
+std::string::size_type lesslen(const std::string &str1, const std::string &str2) {
+    return str1.length() < str2.length() ? str1.length() : str2.length();
+}
+
+std::string::size_type largelen(const std::string &str1, const std::string &str2) {
+    return str1.length() > str2.length() ? str1.length() : str2.length();
+}
+
+decltype(lesslen)* comparefunc(const std::string&) {
+    return lesslen;
+}
+
+void xyf();
+struct xy {
+    friend void xyf() {}
+    xy() { xyf(); }
+    void g();
+    void h();
+};
+
+void xy::g() { xyf(); }
+void xy::h() { xyf(); }
+
+class yy {
+    int x;
+    int y;
+    
+public:
+    yy(int v = 100): y(v), x(y) {}
+    ~yy(void) = default;
+};
+
+class delegate {
+public:
+    int x;
+    int y;
+    int z;
+    
+    delegate(int x, int y, int z): x(x), y(y), z(z) {}
+    delegate(int v): delegate(v, v, v) {}
+};
+
+class ConstDebug {
+public:
+    constexpr ConstDebug(bool b = true): h(b), o(b), t(b) {}
+    constexpr ConstDebug(bool th, bool to, bool tt): h(th), o(to), t(tt) {}
+    
+    constexpr bool any() { return h || o || t; }
+    void seth(bool b) { h = b;}
+    void seto(bool b) { o = b;}
+    void sett(bool b) { t = b;}
+    
+private:
+    bool h;
+    bool o;
+    bool t;
+};
+
 int main(int argc, const char * argv[]) {
     static_assert(has_func<MyStruct>::value, "true");
     //static_assert(has_func<TestStruct>::value, "false");
@@ -95,6 +185,80 @@ int main(int argc, const char * argv[]) {
     std::cout << "Uppercase = " << func(str) << std::endl;
     std::cout << std::is_function<decltype(ooxx)>::value << std::endl;
     std::cout << std::is_function<decltype(TestStruct::poo)>::value << std::endl;
+    
+    test1<10>();
+    //constexpr int cv(test1<10>());
+    std::vector<std::vector<std::string>> strvec;
+    
+    const char vstr[] = "what are you doing";
+    for(auto &ch: vstr) {
+        std::cout << ch << std::endl;
+    }
+    
+    // reference
+    int vx = 10;
+    static_cast<void>(vx = 20), static_cast<void>(30), 40;
+    std::cout << "value = " << vx << std::endl;
+    
+    int  &rfv = vx;
+    int vy = 20;
+    rfv = 100;
+    rfv = vy;
+    std::cout << "value = " << vx << vy << std::endl;
+    
+    // exception
+    std::string str1, str2;
+    /*while(std::cin >> str1 >> str2) {
+        try {
+            if(str1 != str2) {
+                throw std::runtime_error("Not equal");
+            }
+            std::cout << "Equal\n";
+        } catch(std::runtime_error &e) {
+            std::cerr << e.what() << "\nTry Again? Enter y or n" << std::endl;
+            char c;
+            std::cin >> c;
+            if(!std::cin || 'n' == c) {
+                break;
+            }
+        }
+    }*/
+    
+    // mutable parameter
+    std::initializer_list<std::string> ls;
+    
+    // recursive
+    try {
+        std::cout << "fact(100) = " << fact(1000)  << std::endl;
+    } catch (std::underflow_error &e) {
+        std::cout << "underflow = " << e.what() << std::endl;
+    } catch(std::overflow_error &e) {
+        std::cout << "overflow = " <<  e.what() << std::endl;
+    } catch(std::exception &e) {
+        std::cout <<  e.what() << std::endl;
+    }
+    
+    // namespace&overload
+    nsprint(1);
+    nsprint('w');
+    
+    // constexpr
+    constexpr int myfoo = new_sz();
+    
+    // precompiled
+    std::cout << "func = " << __func__ << "\n";
+    std::cout << "file = " << __FILE__ << "\n";
+    std::cout << "line = " << __LINE__ << "\n";
+    std::cout << "time = " << __TIME__ << "\n";
+    std::cout << "date = " << __DATE__ << "\n";
+    
+    // delegating constructor
+    delegate del(10);
+    std::cout << "del = " << del.x << ", " << del.y << ", " << del.z << std::endl;
+    
+    // constexpr constructor
+    constexpr ConstDebug cd(false);
+    cd.seth(true);
     
     return 0;
 }
